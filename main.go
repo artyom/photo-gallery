28e5250c2de21d8e5b050a7c12508f3a4b57039b
Artyom Pervukhin <github@artyom.dev>
2020-08-03 20:57:13 +0300

    Add basic documentation

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
-// TODO describe program
+// Command photo-gallery is a simple web photo gallery generator.
+//
+// It takes a directory with jpeg images (.jpg or .jpeg suffixes) and produces
+// HTML file along with two directories: one holds full-sized copies of
+// original photos, another contains thumbnails. These directories + an HTML
+// file are compatible with any web server supporting static content.
+//
+// The default template produces a self-contained gallery using only HTML and
+// CSS.
 package main
 
 import (
